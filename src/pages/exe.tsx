ee5e9dc6b1220e5d06bdd8dc2b98d8211dbbbf7c
cjescalona1 <cjescalona1@gmail.com>
2023-03-30 02:08:54 -0400

    some images and new BG inset shadow

--- src/pages/exe.tsx
+++ src/pages/exe.tsx
@@ -4,31 +4,31 @@ const ExE = ()=>{
     return(
             <div id="ExE" className={styles.wrapper}>
                 <div className={styles.left}>
-                <div className={styles.slider}>
-	<div className={styles.slideTrack}>
-		<div className={styles.slide}>
-			<img src={"./UWW.jpeg"} height="450" width="350" alt="" />
-		</div>
-		<div className={styles.slide}>
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="450" width="350" alt="" />
-		</div>
-		<div className={styles.slide}>
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="450" width="350" alt="" />
-		</div>
-        
-		 
-		<div className={styles.slide}>
-            <img src={"./UWW.jpeg"} height="450" width="350" alt="" />
-		</div>
-		<div className={styles.slide}>
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="450" width="350" alt="" />
-		</div>
-		<div className={styles.slide}>
-			<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="450" width="350" alt="" />
-		</div>
-		 
-	</div>
-</div>
+                {/* <div className={styles.slider}>
+                <div className={styles.slideTrack}>
+                    <div className={styles.slide}>
+                        <img src={"./UWW.jpeg"} height="450" width="350" alt="" />
+                    </div>
+                    <div className={styles.slide}>
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="450" width="350" alt="" />
+                    </div>
+                    <div className={styles.slide}>
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="450" width="350" alt="" />
+                    </div>
+                    
+                    
+                    <div className={styles.slide}>
+                        <img src={"./UWW.jpeg"} height="450" width="350" alt="" />
+                    </div>
+                    <div className={styles.slide}>
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/2.png" height="450" width="350" alt="" />
+                    </div>
+                    <div className={styles.slide}>
+                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/557257/3.png" height="450" width="350" alt="" />
+                    </div>
+                    
+                </div>
+                </div> */}
                 </div>
                 <div className={styles.right}>
                                         
