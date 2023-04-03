9b7ec867d2a0ea51d00cc0e19fa33afbb9ca6a4c
cjescalona1 <cjescalona1@gmail.com>
2023-04-03 13:46:25 -0400

    first page mobile

--- src/pages/carousel.tsx
+++ src/pages/carousel.tsx
@@ -1,26 +1,29 @@
 import styles from '@/styles/ExE.module.css'
 import Image from 'next/image';
-const carouselElements : string[] = ["car1.jpeg","car2.svg","car3.jpeg","car4.png","car5.png","car6.png","car7.png"];
+const carouselElements: any[] = [{ "description": "Multiporpose NFT related page", "name": "car1.jpeg" }, { "description": "Components integration on E-commerce", "name": "car2.svg" }, { "description": "NFT generative collection", "name": "car3.jpeg" }, { "description": "Mint site for lootboxes related to NFT collection", "name": "car4.png" }, { "description": "Mint site for NFTs", "name": "car5.png" }, { "description": "NFT related staking game", "name": "car6.png" }, { "description": "Cybermetric, Data collection tool", "name": "car7.png" }];
 
-const Carousel = ()=>{
-    return(
+const Carousel = () => {
+    return (
         <>
             <div>
                 <ul className={styles.carouselWrapper}>
-                    {carouselElements.map((item, index)=>{
-                    return(
-                            <li className={styles.itemCarousel} key={item} >
+                    {carouselElements.map((item, index) => {
+                        return (
+                            <li className={styles.itemCarousel} key={item.name} >
                                 <article className={styles.slider}>
                                     <figure >
-                                    <Image id={"id"+index} src={`/${item}`} alt={item} height={400}  width={700}/>
+                                        <Image id={"id" + index} src={`/${item.name}`} alt={item.name} height={400} width={700} />
                                     </figure>
+                                    <figcaption className={styles.auxMar}>
+                                        {item.description}
+                                    </figcaption>
                                 </article>
                             </li>
                         )
-                    })                    
+                    })
                     }
                 </ul>
-                
+
             </div>
 
         </>
